9fc96969a71c0d2a2d7edb1c7bd0f739ea3a8138
ya1303 <yatinsharma1303@gmail.com>
2024-11-22 22:58:12 +0530

    import issue

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -2,13 +2,13 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
-import Homepage from "./routes/homepage/Homepage";
 import DashboardPage from "./routes/dashboardPage/DashboardPage";
 import ChatPage from "./routes/chatPage/ChatPage";
 import RootLayout from "./layouts/rootLayout/RootLayout";
 import DashboardLayout from "./layouts/dashboardLayout/DashboardLayout";
 import SignInPage from "./routes/signInPage/signInPage";
 import SignUpPage from "./routes/signUpPage/signUpPage";
+import Homepage from "./routes/homepage/Homepage";
 
 const router = createBrowserRouter([
   {
