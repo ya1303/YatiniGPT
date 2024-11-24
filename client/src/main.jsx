b45c2b4d040381033a9eb11448c4230213522bff
ya1303 <yatinsharma1303@gmail.com>
2024-11-24 16:12:21 +0530

    Update main.jsx

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
-import Homepage from "./routes/homepage/Homepage"
+import Homepage from "./routes/homepage/Homepage.jsx"
 import ChatPage from "./routes/chatPage/ChatPage";
 import RootLayout from "./layouts/rootLayout/RootLayout";
 import SignInPage from "./routes/signInPage/signInPage";
