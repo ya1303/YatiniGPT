a8cb6578651a20ca619a8ae9d88507922dc82345
ya1303 <yatinsharma1303@gmail.com>
2024-11-24 16:34:36 +0530

    moved chatlist to src

--- client/src/dashboardLayout/DashboardLayout.jsx
+++ client/src/dashboardLayout/DashboardLayout.jsx
@@ -2,7 +2,7 @@ import { Outlet, useNavigate } from "react-router-dom";
 import "./dashboardLayout.css";
 import { useAuth } from "@clerk/clerk-react";
 import { useEffect } from "react";
-import ChatList from "../components/chatList/ChatList";
+import ChatList from "../chatList/ChatList";
 
 const DashboardLayout = () => {
   const { userId, isLoaded } = useAuth();
